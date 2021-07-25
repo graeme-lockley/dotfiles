120d0bf2e827020ace5ec6885fe3c1ee767b60e8
Graeme Lockley <graeme.lockley@investec.co.za>
2021-07-25 17:13:20 +0200

    refactor: move email address out into the setup files

--- modules/git/_setup.zsh
+++ modules/git/_setup.zsh
@@ -4,12 +4,6 @@
 
 mkdir -p ~/.config
 
-if [[ "$DOTFILES_PERSONALITY" -eq "work" ]]; then
-    EMAIL_ADDRESS=graeme.lockley@investec.co.za
-else
-    EMAIL_ADDRESS=graeme.lockley@gmail.com
-fi
-
 sed "s/EMAILADDRESS/$EMAIL_ADDRESS/g" $DOTFILES_HOME/modules/git/config.template  > $DOTFILES_HOME/modules/git/git/config
 
 ln -s -f $DOTFILES_HOME/modules/git/git ~/.config
--- settings/_base-setup.zsh
+++ settings/_base-setup.zsh
@@ -1,6 +1,7 @@
 # The default personality is home setup.  This setting can then be used by all 
 # scripts to ensure a context aware setup.
 export DOTFILES_PERSONALITY=home
+export EMAIL_ADDRESS=graeme.lockley@gmail.com
 
 export DOTFILES_MODULES=(
     # homebrew
