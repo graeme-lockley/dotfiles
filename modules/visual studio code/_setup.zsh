83db20b56a5666bb3cb77d00edcd1edadf69cb04
Graeme Lockley <graeme.lockley@investec.co.za>
2021-08-01 15:39:11 +0200

    feature: add spellchecker to vscode

--- modules/visual studio code/_setup.zsh
+++ modules/visual studio code/_setup.zsh
@@ -10,3 +10,4 @@ code --force --install-extension ms-azuretools.vscode-docker
 code --force --install-extension coenraads.bracket-pair-colorizer-2
 code --force --install-extension ionide.ionide-fsharp
 code --force --install-extension foxundermoon.shell-format
+code --force --install-extension streetsidesoftware.code-spell-checker
