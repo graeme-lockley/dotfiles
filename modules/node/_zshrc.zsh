995922871353662733f4c32dc07fafbdf10295fe
Graeme Lockley <graeme.lockley@gmail.com>
2022-02-14 06:51:32 +0200

    fix: update node path based on arch

--- modules/node/_zshrc.zsh
+++ modules/node/_zshrc.zsh
@@ -1,1 +1,6 @@
-export PATH="/opt/homebrew/opt/node@14/bin:$PATH"
+if [[ "$(uname -p)" == "arm" ]]; then
+    export PATH="/opt/homebrew/opt/node@14/bin:$PATH"
+else
+    export PATH="/usr/local/opt/node@14/bin:$PATH"
+fi
+
