5760327b853b76a69c9b0bd753f7167c9f5c406d
Graeme Lockley <graeme.lockley@gmail.com>
2022-02-20 12:53:46 +0200

    feature: install azure cli

--- modules/azure-tools/_setup.zsh
+++ modules/azure-tools/_setup.zsh
@@ -1,4 +1,10 @@
 #!/bin/zsh
 
+if `$DOTFILES_HOME/bin/is-executable az`; then
+    az upgrade
+else
+    brew install azure-cli
+fi
+
 brew tap azure/functions
 brew install azure-functions-core-tools@4
