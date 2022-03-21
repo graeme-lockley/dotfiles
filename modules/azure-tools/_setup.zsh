d7f11000de2c232166f68c489975824ab0fd8771
Graeme Lockley <graeme.lockley@gmail.com>
2022-03-21 07:03:31 +0200

    feature: upgrade az bicep

--- modules/azure-tools/_setup.zsh
+++ modules/azure-tools/_setup.zsh
@@ -2,9 +2,13 @@
 
 if `$DOTFILES_HOME/bin/is-executable az`; then
     az upgrade
+    az bicep upgrade
 else
     brew install azure-cli
 fi
 
 brew tap azure/functions
 brew install azure-functions-core-tools@4
+
+brew tap azure/bicep
+brew install bicep
